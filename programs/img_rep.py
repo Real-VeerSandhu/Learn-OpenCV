9f6907cae4f47366363fc9c859d4b0801f52c49d
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-06-17 09:03:34 -0400

    Modified Image

--- programs/img_rep.py
+++ programs/img_rep.py
@@ -7,3 +7,11 @@ print(f'Type: {type(image)}')
 print(f'Shape: {image.shape}') # 256, 256, 3
 
 print(f'First pixel: {image[0][0]}')
+
+for i in range(100): # Number of rows to change (max 256 for bottle.jpg)
+    for j in range(image.shape[1]):
+        image[i][j] = [233, 45, 23]
+
+cv2.imshow('Modified Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
